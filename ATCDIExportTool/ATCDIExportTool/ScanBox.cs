697e3668b2df4af4ecbc6f38a1cb26cccc439525
Shi Rui <122141976@qq.com>
2021-11-04 11:26:57 +0800

    update 21/11/4

--- ATCDIExportTool/ATCDIExportTool/ScanBox.cs
+++ ATCDIExportTool/ATCDIExportTool/ScanBox.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.Collections.Generic;
 
 namespace ATCDIExportTool
 {
     public partial class ScanBox : Form
     {
+        private ScanTool tool;
         public ScanBox()
         {
             InitializeComponent();
             this.TopMost = true;
             this.Show();
+            this.tool = new ScanTool();
         }
 
         public static void StartExportTool(string unparsed)
@@ -26,13 +23,37 @@ namespace ATCDIExportTool
 
         private void ScanButton_Click(object sender, EventArgs e)
         {
-            ScanTool tool = new ScanTool();
-            tool.StartScan();
+            this.ScanText.Text = tool.StartScan();
         }
 
         private void ExportCenter_Click(object sender, EventArgs e)
         {
-
+            if (this.tool.export.elements.Count == 0)
+            {
+                MessageBox.Show("请重新执行扫描");
+            }
+            else
+            {
+                SaveFileDialog dialog = new SaveFileDialog()
+                {
+                    Title = "选择保存文件位置",
+                    Filter = "Excel|*.xlsx",
+                    FileName = "构件中心点"
+                };
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    XLWorkbook xls = new XLWorkbook();
+                    IXLWorksheet sheet = xls.AddWorksheet();
+                    sheet.Cell(1, 1).InsertData(new List<string>() { "guid", "elementId", "x", "y", "z" }, true);
+                    foreach (ExportElement el in this.tool.export.elements)
+                    {
+                        sheet.LastRowUsed().RowBelow().Cell(1).InsertData(
+                            new List<object>(){ el.guid, el.elementId, el.center.X, el.center.Y, el.center.Z}, true);
+                    }
+                    xls.SaveAs(dialog.FileName);
+                    MessageBox.Show("导出完成");
+                }
+            }
         }
 
         private void ExportModel_Click(object sender, EventArgs e)
--- ATCDIExportTool/ATCDIExportTool/ScanTool.cs
+++ ATCDIExportTool/ATCDIExportTool/ScanTool.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using Bentley.DgnPlatformNET;
 using Bentley.DgnPlatformNET.Elements;
 using Bentley.MstnPlatformNET;
 using Bentley.GeometryNET;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace ATCDIExportTool
 {
@@ -25,27 +26,54 @@ namespace ATCDIExportTool
             "MeshHeader"
         };
         private Dictionary<IntPtr, DgnModel> models = new Dictionary<IntPtr, DgnModel>();
-        private ExportFile export = new ExportFile();
+        public ExportFile export = new ExportFile();
 
-        public void StartScan()
+        public string StartScan()
         {
-            this.models.Clear();
-            this.export.Reset();
+            models.Clear();
+            export.Reset();
             ConfirmModel(Session.Instance.GetActiveDgnModel());
+            Dictionary<string, List<string>> ignore = new Dictionary<string, List<string>>();
+            Dictionary<string, List<string>> confirm = new Dictionary<string, List<string>>();
+
             foreach ( DgnModel model in models.Values)
             {
                 foreach (Element el in model.GetGraphicElements())
                 {
-                    if (el.IsValid && !el.IsInvisible && this.ElementTypeFilter.Contains(el.ElementType.ToString()))
+                    string id = el.ElementId.ToString();
+                    string type = el.ElementType.ToString();
+                    if (el.IsValid && !el.IsInvisible && ElementTypeFilter.Contains(type))
                     {
-                        this.export.AddElement(el);
+                        export.AddElement(el);
+                        if (confirm.ContainsKey(type))
+                        {
+                            confirm[type].Add(id);
+                        }
+                        else
+                        {
+                            confirm.Add(type, new List<string>() { id });
+                        }
                     }
                     else
                     {
-                        MessageCenter.Instance.ShowInfoMessage("忽略元素说明", "", false);
+                        if (ignore.ContainsKey(type))
+                        {
+                            ignore[type].Add(id);
+                        }
+                        else
+                        {
+                            ignore.Add(type, new List<string>() {id });
+                        }
                     }
                 }
             }
+            MessageCenter.Instance.ShowInfoMessage("忽略元素详细 ", 
+                JsonConvert.SerializeObject(ignore.Select(x=> x.Key + " : " + string.Join(", ", x.Value)), Formatting.Indented), false);
+            MessageCenter.Instance.ShowInfoMessage("选中元素详细",
+                JsonConvert.SerializeObject(confirm.Select(x => x.Key + " : " + string.Join(", ", x.Value)), Formatting.Indented), false);
+
+            return "total : " + confirm.Select(x => x.Value.Count).Sum() + Environment.NewLine +
+                JsonConvert.SerializeObject(confirm.Select(x => x.Key + " : " + x.Value.Count), Formatting.Indented);
         }
 
         private void ConfirmModel(DgnModel model)
@@ -71,11 +99,13 @@ namespace ATCDIExportTool
         public readonly Element element;
         public readonly DPoint3d center;
         public readonly string guid;
+        public readonly string elementId;
         public ExportElement(Element el)
         {
-            this.element = el;
-            this.center = Utils.GetElementCenter(el);
-            this.guid = Utils.GetElementGuid(el);
+            element = el;
+            center = Utils.GetElementCenter(el);
+            guid = Utils.GetElementGuid(el);
+            elementId = el.ElementId.ToString();
         }
 
     }
@@ -98,9 +128,9 @@ namespace ATCDIExportTool
 
         public void Reset()
         {
-            this.elements.Clear();
-            this.center = DPoint3d.Zero;
-            this.range = DRange3d.NullRange;
+            elements.Clear();
+            center = DPoint3d.Zero;
+            range = DRange3d.NullRange;
             Session.Instance.GetActiveDgnModel().GetRange(out range);
             range.High = Utils.ConvertUorToMeter(range.High);
             range.Low = Utils.ConvertUorToMeter(range.Low);
